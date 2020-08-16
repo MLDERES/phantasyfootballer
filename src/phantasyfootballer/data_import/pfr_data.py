8e6eace876fa1b48acf649d5f25121b48efb5ede
Michael Dereszynski <mlderes@hotmail.com>
2020-08-16 21:40:38 +0000

    get rushing and receiving data from pro fball ref

--- src/phantasyfootballer/data_import/pfr_data.py
+++ src/phantasyfootballer/data_import/pfr_data.py
@@ -7,10 +7,16 @@ def _make_path(year, stat_group):
     return f'{PRO_FOOTBALL_URL}/years/{year}/{stat_group}.htm'
    
 
-def fetch_pfr_data_by_year(start_year, stat_group, xpath):
+def fetch_pfr_data_by_year(start_year, stat_group, **read_args):
     uri = _make_path(start_year, stat_group)
-    print(f'Attempt to retrieve data from {uri} on Xpath {xpath}')
-    return pd.read_html(uri, xpath, index_col='Player')[0]
+    print(f'Attempt to retrieve data from {uri}')
+    return pd.read_html(uri, **read_args)[0]
 
 def fetch_passing_data(start_year):
-    return fetch_pfr_data_by_year(start_year, 'passing', 'Passing Table')
+    return fetch_pfr_data_by_year(start_year, 'passing', attrs={'id':'Passing Table'})
+
+def fetch_rushing_data(start_year):
+    return fetch_pfr_data_by_year(start_year, 'rushing', attrs={'id':'rushing'}, header=1)
+
+def fetch_receiving_data(start_year):
+    return fetch_pfr_data_by_year(start_year, 'receiving', attrs={'id':'receiving'})
